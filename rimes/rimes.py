c9b377e968e3c5c2952881991af2575f52d6b16b
Julien Rouzieres <julien.rouzieres@mac.com>
2019-09-03 00:33:48 +0200

    operationnal loop

--- rimes/rimes.py
+++ rimes/rimes.py
@@ -1,37 +1,38 @@
 #!/usr/bin/env python
 
 import argparse
-import os
+import colorama
 import pkg_resources
-import re
-import unicodedata
 
 
 def arg_parser():
     parser = argparse.ArgumentParser(description="A simple tool for poets")
     parser.add_argument(
-        "ending", metavar="ENDING", type=str, nargs="*", help="the end of the word"
+        "ending", metavar="ENDING", type=str, nargs="*", help="end of the word"
     )
     return parser
 
 
 def runner():
+    colorama.init()
     parser = arg_parser()
     args = vars(parser.parse_args())
 
-    endings = [re.compile(f"{ending}$", re.IGNORECASE) for ending in args["ending"]]
+    words = []
+    with open(pkg_resources.resource_filename(__name__, "data/fr")) as reader:
+        for line in reader:
+            word = line.strip()
+            for ending in args["ending"]:
+                if word.endswith(ending):
+                    words.append(
+                        word[: -len(ending)]
+                        + colorama.Fore.BLUE
+                        + ending
+                        + colorama.Style.RESET_ALL
+                    )
+                    break
 
-    print(list(filter(lambda exp: exp.match("cacatot"), endings)))
-
-    filename = pkg_resources.resource_filename(__name__, "data/fr")
-    with open(filename) as fd:
-        words = [
-            word.strip()
-            for word in fd.readlines()
-            if len(list(filter(lambda exp: exp.match(exp, word.strip()), endings))) > 0
-        ]
-
-    print(words)
+    print(" ".join(words))
 
 
 if __name__ == "__main__":
