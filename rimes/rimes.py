1e191e7ea20f8c6318aa4056631434b1b6cdbdf5
julienrouzieres <julien.rouzieres@mac.com>
2019-09-03 16:25:23 +0200

    working version

--- rimes/rimes.py
+++ rimes/rimes.py
@@ -1,38 +1,58 @@
 #!/usr/bin/env python
 
 import argparse
-import colorama
-import pkg_resources
+from colorama import init, Fore, Style
+from pkg_resources import resource_filename, resource_listdir
+import random
+import os
 
 
 def arg_parser():
     parser = argparse.ArgumentParser(description="A simple tool for poets")
+
     parser.add_argument(
-        "ending", metavar="ENDING", type=str, nargs="*", help="end of the word"
+        "ending", metavar="ENDING", type=str, nargs="+", help="end of the word"
     )
+    parser.add_argument(
+        "-l",
+        "--lang",
+        type=str,
+        choices=resource_listdir(__name__, "data"),
+        default="fr",
+        help="used language",
+    )
+    parser.add_argument("-r", "--rand", type=int, help="pick random words")
+
     return parser
 
 
 def runner():
-    colorama.init()
+    init()
+
     parser = arg_parser()
-    args = vars(parser.parse_args())
+    args = parser.parse_args()
 
     words = []
-    with open(pkg_resources.resource_filename(__name__, "data/fr")) as reader:
+
+    filename = resource_filename(__name__, os.path.join("data", args.lang))
+    with open(filename) as reader:
         for line in reader:
             word = line.strip()
-            for ending in args["ending"]:
+            for arg in args.ending:
+                ending = arg.lower()
                 if word.endswith(ending):
                     words.append(
-                        word[: -len(ending)]
-                        + colorama.Fore.BLUE
-                        + ending
-                        + colorama.Style.RESET_ALL
+                        word[: -len(ending)] + Fore.BLUE + ending + Style.RESET_ALL
                     )
                     break
 
-    print(" ".join(words))
+    if words:
+        if args.rand is not None:
+            if len(words) > args.rand:
+                words = random.sample(words, args.rand)
+
+        print(" ".join(words))
+
 
 
 if __name__ == "__main__":
