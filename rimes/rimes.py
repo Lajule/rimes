27a8ebadf72a0ae7eadd7b429312f5fd23a8f230
julienrouzieres <julien.rouzieres@mac.com>
2019-09-04 15:48:16 +0200

    introduce colors and remove stupid pretty printer

--- rimes/rimes.py
+++ rimes/rimes.py
@@ -1,29 +1,20 @@
 #!/usr/bin/env python
 
 import argparse
+from colorama import init, Fore, Style
 import os
 from pkg_resources import resource_filename, resource_listdir
-import pprint
 import random
-import shutil
 
 
 def arg_parser():
     parser = argparse.ArgumentParser(description="A simple tool for poets")
 
+    parser.add_argument("ending", metavar="ENDING", type=str, nargs="+", help="end of the word")
+    parser.add_argument("-c", "--compact", action="store_true", help="display compacted words")
+    parser.add_argument("-n", "--no-color", action="store_false", help="display uncolored words")
     parser.add_argument(
-        "ending", metavar="ENDING", type=str, nargs="+", help="end of the word"
-    )
-    parser.add_argument(
-        "-c", "--compact", action="store_true", help="display compacted words"
-    )
-    parser.add_argument(
-        "-l",
-        "--lang",
-        type=str,
-        choices=resource_listdir(__name__, "data"),
-        default="fr",
-        help="used language",
+        "-l", "--lang", type=str, choices=resource_listdir(__name__, "data"), default="fr", help="used language"
     )
     parser.add_argument("-r", "--rand", type=int, help="pick random words")
 
@@ -31,30 +22,31 @@ def arg_parser():
 
 
 def runner():
+    init()
+    colors = [Fore.RED, Fore.GREEN, Fore.BLUE, Fore.YELLOW, Fore.MAGENTA, Fore.CYAN]
+
     parser = arg_parser()
     args = parser.parse_args()
 
     words = []
+
     filename = resource_filename(__name__, os.path.join("data", args.lang))
     with open(filename) as reader:
         for line in reader:
             word = line.strip()
-            for arg in args.ending:
+            for i, arg in enumerate(args.ending):
                 ending = arg.lower()
                 if word.endswith(ending):
-                    words.append(word)
+                    words.append(
+                        word[: -len(ending)] + Style.BRIGHT + colors[i % len(colors)] + ending + Style.RESET_ALL
+                        if args.no_color
+                        else word
+                    )
                     break
 
     if words:
-        columns, _ = shutil.get_terminal_size()
-        output = pprint.pformat(
-            random.sample(words, args.rand)
-            if args.rand is not None and len(words) > args.rand
-            else words,
-            width=columns,
-            compact=args.compact,
-        )
-        print(output)
+        sep = "\t" if args.compact else "\n"
+        print(sep.join(random.sample(words, args.rand) if args.rand is not None and len(words) > args.rand else words))
 
 
 if __name__ == "__main__":
