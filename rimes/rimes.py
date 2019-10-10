a521b8dd45166b3d69e4898aa05e6eed287bcb37
Julien ROUZIERES <julien.rouzieres@mac.com>
2019-10-10 13:59:07 +0200

    Switch to click

--- rimes/rimes.py
+++ rimes/rimes.py
@@ -1,46 +1,47 @@
 #!/usr/bin/env python
 
-import argparse
+import click
 from colorama import init, Fore, Style
 import os
 from pkg_resources import resource_filename, resource_listdir
 import random
 
 
-def do_it():
+@click.command()
+@click.option("-c", "--compact", is_flag=True, help="display compacted words")
+@click.option("-n", "--no-color", is_flag=True, default=True, help="display uncolored words")
+@click.option("-r", "--rand", type=int, help="pick random words")
+@click.option("-l", "--lang", type=click.Choice(resource_listdir(__name__, "data")), default="fr", help="used language")
+@click.argument("endings", type=str, nargs=-1)
+def do_it(compact, no_color, rand, lang, endings):
+    """A simple tool for poets."""
+
     init()
     colors = [Fore.RED, Fore.GREEN, Fore.BLUE, Fore.YELLOW, Fore.MAGENTA, Fore.CYAN]
 
-    languages = resource_listdir(__name__, "data")
-
-    parser = argparse.ArgumentParser(description="A simple tool for poets")
-    parser.add_argument("ending", metavar="ENDING", type=str, nargs="+", help="end of the word")
-    parser.add_argument("-c", "--compact", action="store_true", help="display compacted words")
-    parser.add_argument("-n", "--no-color", action="store_false", help="display uncolored words")
-    parser.add_argument("-l", "--lang", type=str, choices=languages, default="fr", help="used language")
-    parser.add_argument("-r", "--rand", type=int, help="pick random words")
-
-    args = parser.parse_args()
-
     words = []
 
-    filename = resource_filename(__name__, os.path.join("data", args.lang))
+    filename = resource_filename(__name__, os.path.join("data", lang))
     with open(filename) as reader:
         for line in reader:
             word = line.strip()
-            for i, arg in enumerate(args.ending):
-                ending = arg.lower()
-                if word.endswith(ending):
+            for i, ending in enumerate(endings):
+                lower_ending = ending.lower()
+                if word.endswith(lower_ending):
                     words.append(
-                        word[: -len(ending)] + Style.BRIGHT + colors[i % len(colors)] + ending + Style.RESET_ALL
-                        if args.no_color
+                        word[: -len(lower_ending)]
+                        + Style.BRIGHT
+                        + colors[i % len(colors)]
+                        + lower_ending
+                        + Style.RESET_ALL
+                        if no_color
                         else word
                     )
                     break
 
     if words:
-        sep = "\t" if args.compact else "\n"
-        print(sep.join(random.sample(words, args.rand) if args.rand is not None and len(words) > args.rand else words))
+        sep = "\t" if compact else "\n"
+        click.echo(sep.join(random.sample(words, rand) if rand is not None and len(words) > rand else words))
 
 
 if __name__ == "__main__":
