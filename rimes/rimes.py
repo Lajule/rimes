aa80781a31c2f061f632637e9668ee38addb3120
Julien ROUZIERES <julien.rouzieres@mac.com>
2019-10-10 10:09:04 +0200

    Famous do_it function

--- rimes/rimes.py
+++ rimes/rimes.py
@@ -7,25 +7,19 @@ from pkg_resources import resource_filename, resource_listdir
 import random
 
 
-def arg_parser():
+def do_it():
+    init()
+    colors = [Fore.RED, Fore.GREEN, Fore.BLUE, Fore.YELLOW, Fore.MAGENTA, Fore.CYAN]
+
     languages = resource_listdir(__name__, "data")
 
     parser = argparse.ArgumentParser(description="A simple tool for poets")
-
     parser.add_argument("ending", metavar="ENDING", type=str, nargs="+", help="end of the word")
     parser.add_argument("-c", "--compact", action="store_true", help="display compacted words")
     parser.add_argument("-n", "--no-color", action="store_false", help="display uncolored words")
     parser.add_argument("-l", "--lang", type=str, choices=languages, default="fr", help="used language")
     parser.add_argument("-r", "--rand", type=int, help="pick random words")
 
-    return parser
-
-
-def runner():
-    init()
-    colors = [Fore.RED, Fore.GREEN, Fore.BLUE, Fore.YELLOW, Fore.MAGENTA, Fore.CYAN]
-
-    parser = arg_parser()
     args = parser.parse_args()
 
     words = []
@@ -50,4 +44,4 @@ def runner():
 
 
 if __name__ == "__main__":
-    runner()
+    do_it()
